a8eb0fc6f2634d5c67703bcc702953f1f5d32590
paulostevo <41132104+paulostevo@users.noreply.github.com>
2023-01-07 12:22:22 -0300

    Configuring web microservices for consuming productmicroservices

--- GeekShopping.Web/Program.cs
+++ GeekShopping.Web/Program.cs
@@ -1,15 +1,23 @@
+using GeekShopping.Web.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+builder.Services.AddHttpClient<ProductService, ProductService>(c =>
+        c.BaseAddress = new Uri(builder.Configuration["ServiceUrls:ProductAPI"]));
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
 }
+
 app.UseStaticFiles();
 
 app.UseRouting();
